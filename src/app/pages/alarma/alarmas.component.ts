00b0ad001cd52bfdab150140ee9b3bef803b29b7
Renata <131387800+renamilla@users.noreply.github.com>
2023-10-01 00:04:52 -0300

    Add files via upload

---
+++ src/app/pages/alarma/alarmas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alarmas',
+  templateUrl: './alarmas.component.html',
+  styleUrls: ['./alarmas.component.css']
+})
+export class AlarmasComponent {
+
+}
